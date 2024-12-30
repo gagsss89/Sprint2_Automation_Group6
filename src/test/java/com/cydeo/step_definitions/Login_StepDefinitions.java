3b4fadfb707c1f6cb5940798c4da22ab72204880
firlenysfeliz <firlenys_feliz@hotmail.com>
2024-12-30 00:11:31 -0500

    Login TC2

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -3,6 +3,7 @@ package com.cydeo.step_definitions;
 import com.cydeo.pages.LoginPage;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -28,6 +29,30 @@ public class Login_StepDefinitions {
     public void user_lands_on_home_page() {
         String expectedUrl = "https://qa.symmetrix.io/stream/?login=yes";
         Assert.assertEquals(expectedUrl,Driver.getDriver().getCurrentUrl());
+
+    }
+
+    @Given("user is in login page")
+    public void userIsInLoginPage() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("envURL"));
+    }
+
+    @When("user enter invalid {string} or {string}")
+    public void userEnterInvalidOr(String username, String password) {
+        loginPage.username.sendKeys(username);
+        loginPage.password.sendKeys(password);
+    }
+
+    @And("user click login button")
+    public void userClickLoginButton() {
+        loginPage.loginButton.click();
+    }
+
+    @Then("user get an error message for invalid login attempt")
+    public void userGetAnErrorMessageForInvalidLoginAttempt() {
+        String expectedMessage = "Incorrect login or password";
+        String actualMessage = "Incorrect login or password";
+        Assert.assertEquals(expectedMessage,actualMessage);
 
     }
 
